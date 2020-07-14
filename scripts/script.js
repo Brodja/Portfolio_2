86f00d74b7c532b90091c4c42dc3f87ffdc52c83
Bohdan Derkach <body1906@gmail.com>
2020-07-14 14:47:14 +0300

    v1.1

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed:1500,
   } );
+  $('.hover').on('touchstart touchend', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('hover_effect');
+});
 });
